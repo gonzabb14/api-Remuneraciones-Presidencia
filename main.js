b23e24a6baf4283c37bda486da47c2b37f9b8a1b
Gonzalo Maulella <gonzavv14@gmail.com>
2023-09-07 20:42:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-const API_URL = "https://catalogodatos.gub.uy/dataset/13b9db1b-2f57-472f-aaa9-2906de549aa9/resource/3e2e8ff8-832e-4483-bc14-09056ef51dcb/download/envios-postales.json";
+const API_URL = "https://randomuser.me/api/";
 
 fetch(API_URL)
-    .then(response => {
-        console.log(response);
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.results[0]);
     })
     .catch(error => {
         console.log(error);
       });
-
-      
